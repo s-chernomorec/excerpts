95bd092771e8eeecd9571a3509eb372b91ffb1ea
Sergey Chernomorets <srg.chernomorec@gmail.com>
2017-07-09 06:20:38 +0300

    added link to this repository

--- src/js/components/pages/About.js
+++ src/js/components/pages/About.js
@@ -7,7 +7,7 @@ class About extends Component {
     return (
       <Page headerTitle="About" cls="about-page">
         <p className="about-paragraph">This is an open source project which allows you to save notes, categorize them and access whenever needed.</p>
-        <p className="about-paragraph">All source code is avaible on github: </p>
+        <p className="about-paragraph">All source code is avaible on github: <a className="about-link" href="https://github.com/s-chernomorec/excerpts">https://github.com/s-chernomorec/excerpts</a></p>
         <p className="about-paragraph">Built using React framework: <a className="about-link" href="https://facebook.github.io/react/">https://facebook.github.io/react/</a></p>
       </Page>
     );
